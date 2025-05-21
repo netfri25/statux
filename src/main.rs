0d0a86a87fa913c53a60f16a69c183947742b254
Netanel Friedman <netanel.friedman6@gmail.com>
2025-05-21 13:53:46 +0300

    Use single threaded tokio runtime

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,29 @@ use std::time::Duration;
 mod component;
 mod context;
 
-#[tokio::main]
-async fn main() {
-    Context::new()
-        .add_static("CPU")
-        .add_timed(Duration::from_secs(1), CpuUsage::new())
-        .seperator()
-        .add_static("RAM")
-        .add_timed(Duration::from_secs_f32(0.5), RamUsed::new())
-        .seperator()
-        .add_static("TIME")
-        .add_timed(Duration::from_secs(5), Time::new("%S"))
-        .seperator()
-        .add_static("DISK")
-        .add_timed(Duration::from_secs(30), DiskUsage::new("/"))
-        .seperator()
-        .add_static("WIFI")
-        .add_timed(Duration::from_secs(3), NetworkSSID)
-        .run().await;
+fn main() {
+    let body = async {
+        Context::new()
+            .add_static("CPU")
+            .add_timed(Duration::from_secs(1), CpuUsage::new())
+            .seperator()
+            .add_static("RAM")
+            .add_timed(Duration::from_secs_f32(0.5), RamUsed::new())
+            .seperator()
+            .add_static("TIME")
+            .add_timed(Duration::from_secs(5), Time::new("%S"))
+            .seperator()
+            .add_static("DISK")
+            .add_timed(Duration::from_secs(30), DiskUsage::new("/"))
+            .seperator()
+            .add_static("WIFI")
+            .add_timed(Duration::from_secs(3), NetworkSSID)
+            .run().await;
+    };
+
+    tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .expect("Failed building the Runtime")
+        .block_on(body)
 }
